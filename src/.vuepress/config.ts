463ff986a550431d2e2a0f5e76e0343a69586a25
Shenxinjie <1574650522@qq.com>
2025-08-05 17:48:03 +0800

    test

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import {defineUserConfig} from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-    base: "/CrzyKaiSa/",
+    base: "/CrazyBlob/",
 
     lang: "zh-CN",
     title: "CrazyKaiSa",
