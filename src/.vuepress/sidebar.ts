2ed5bb8d23d48dca965525adec410883840552f5
Shenxinjie <1574650522@qq.com>
2025-12-17 14:47:48 +0800

    feat：1. nest

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -1,77 +1,84 @@
 import {sidebar} from 'vuepress-theme-hope'
 // https://icon-sets.iconify.design/
 export default sidebar({
-    '/': [
-        '',
-        {
-            text: '如何使用',
-            icon: 'laptop-code',
-            prefix: 'demo/',
-            link: 'demo/',
-            children: 'structure',
-        },
-        {
-            text: '文章',
-            icon: 'book',
-            prefix: 'posts/',
-            children: 'structure',
-        },
-        'intro',
-        {
-            text: '幻灯片',
-            icon: 'person-chalkboard',
-            link: 'https://ecosystem.vuejs.press/zh/plugins/markdown/revealjs/demo.html',
-        },
-    ],
-    '/learn/': [
-        {
-            text: '项目实战积累',
-            icon: 'eos-icons:project',
-            prefix: 'Project/',
-            link: 'Project/',
-            children: 'structure',
-        },
-        {
-            text: 'JavaScript',
-            icon: 'vscode-icons:file-type-js-official',
-            prefix: 'JavaScript/',
-            link: 'JavaScript/',
-            children: 'structure',
-        },
-        {
-            text: 'TypeScript',
-            icon: 'vscode-icons:file-type-typescript-official',
-            prefix: 'TypeScript/',
-            link: 'TypeScript/',
-            children: 'structure',
-        },
-        {
-            text: 'Vue3',
-            icon: 'vscode-icons:file-type-vue',
-            prefix: 'Vue3/',
-            link: 'Vue3/',
-            children: 'structure',
-        },
-        {
-            text: 'React',
-            icon: 'vscode-icons:file-type-reactjs',
-            prefix: 'React/',
-            link: 'React/',
-            children: 'structure',
-        },
-        {
-            text: 'Webpack',
-            icon: 'vscode-icons:file-type-webpack',
-            prefix: 'Webpack/',
-            link: 'Webpack/',
-            children: 'structure',
-        },
-        {
-            text: 'Git',
-            icon: 'vscode-icons:file-type-git',
-            prefix: 'Git/',
-            link: 'Git/',
-            children: 'structure',
-        }
-    ]
+  '/': [
+    '',
+    {
+      text: '如何使用',
+      icon: 'laptop-code',
+      prefix: 'demo/',
+      link: 'demo/',
+      children: 'structure',
+    },
+    {
+      text: '文章',
+      icon: 'book',
+      prefix: 'posts/',
+      children: 'structure',
+    },
+    'intro',
+    {
+      text: '幻灯片',
+      icon: 'person-chalkboard',
+      link: 'https://ecosystem.vuejs.press/zh/plugins/markdown/revealjs/demo.html',
+    },
+  ],
+  '/learn/': [
+    {
+      text: '项目实战积累',
+      icon: 'eos-icons:project',
+      prefix: 'Project/',
+      link: 'Project/',
+      children: 'structure',
+    },
+    {
+      text: 'JavaScript',
+      icon: 'vscode-icons:file-type-js-official',
+      prefix: 'JavaScript/',
+      link: 'JavaScript/',
+      children: 'structure',
+    },
+    {
+      text: 'TypeScript',
+      icon: 'vscode-icons:file-type-typescript-official',
+      prefix: 'TypeScript/',
+      link: 'TypeScript/',
+      children: 'structure',
+    },
+    {
+      text: 'Vue3',
+      icon: 'vscode-icons:file-type-vue',
+      prefix: 'Vue3/',
+      link: 'Vue3/',
+      children: 'structure',
+    },
+    {
+      text: 'React',
+      icon: 'vscode-icons:file-type-reactjs',
+      prefix: 'React/',
+      link: 'React/',
+      children: 'structure',
+    },
+    {
+      text: 'NestJS',
+      icon: 'vscode-icons:file-type-nestjs',
+      prefix: 'NestJS/',
+      link: 'NestJS/',
+      children: 'structure',
+    },
+    {
+      text: 'Webpack',
+      icon: 'vscode-icons:file-type-webpack',
+      prefix: 'Webpack/',
+      link: 'Webpack/',
+      children: 'structure',
+    },
+    {
+      text: 'Git',
+      icon: 'vscode-icons:file-type-git',
+      prefix: 'Git/',
+      link: 'Git/',
+      children: 'structure',
+    }
+  ]
 })
--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -4,227 +4,224 @@ import navbar from './navbar.js'
 import sidebar from './sidebar.js'
 
 export default hopeTheme({
-    hostname: 'https://mister-hope.github.io',
+  hostname: 'https://mister-hope.github.io',
 
-    author: {
-        name: 'Mr shen',
-        url: 'https://mister-hope.com',
+  author: {
+    name: 'Mr shen',
+    url: 'https://mister-hope.com',
+  },
+
+  logo: '/assets/icon/touxiang.jpg',
+
+  repo: 'shenxinjie1127/CrazyBlob',
+
+  docsDir: 'src',
+
+  // 导航栏
+  navbar,
+
+  // 侧边栏
+  sidebar,
+
+  // 页脚
+  footer: '默认页脚',
+  displayFooter: true,
+
+  // 博客相关
+  blog: {
+    description: '一个前端开发者',
+    intro: '/intro.html',
+    medias: {
+      Gitee: 'https://gitee.com/shenxinjie1127',
+      GitHub: 'https://github.com/shenxinjie1127',
+
     },
+  },
 
-    logo: '/assets/icon/touxiang.jpg',
+  // 加密配置
+  encrypt: {
+    config: {
+      '/demo/encrypt.html': {
+        hint: 'Password: 1234',
+        password: '1234',
+      },
+      '/learn/Project/5.html': {
+        hint: "机密",
+        password: "112700"
+      },
+      '/learn/Project/6.html': {
+        hint: "机密",
+        password: "112700"
+      },
+      '/learn/Project/7.html': {
+        hint: "机密",
+        password: "112700"
+      }
+    },
+  },
 
-    repo: 'shenxinjie1127/CrazyBlob',
+  // 多语言配置
+  metaLocales: {
+    editLink: '在 GitHub 上编辑此页',
+  },
 
-    docsDir: 'src',
+  // 如果想要实时查看任何改变，启用它。注: 这对更新性能有很大负面影响
+  // hotReload: true,
 
-    // 导航栏
-    navbar,
-
-    // 侧边栏
-    sidebar,
-
-    // 页脚
-    footer: '默认页脚',
-    displayFooter: true,
-
-    // 博客相关
-    blog: {
-        description: '一个前端开发者',
-        intro: '/intro.html',
-        medias: {
-            Gitee: 'https://gitee.com/shenxinjie1127',
-            GitHub: 'https://github.com/shenxinjie1127',
-            Vue3: {
-                icon: 'logos:vue',
-                link: 'https://github.com/shenxinjie1127'
+  // 此处开启了很多功能用于演示，你应仅保留用到的功能。
+  markdown: {
+    align: true,
+    attrs: true,
+    codeTabs: true,
+    component: true,
+    demo: true,
+    figure: true,
+    gfm: true,
+    imgLazyload: true,
+    imgSize: true,
+    include: true,
+    mark: true,
+    plantuml: true,
+    spoiler: true,
+    stylize: [
+      {
+        matcher: 'Recommended',
+        replacer: ({tag}) => {
+          if (tag === 'em')
+            return {
+              tag: 'Badge',
+              attrs: {type: 'tip'},
+              content: 'Recommended',
             }
         },
+      },
+    ],
+    sub: true,
+    sup: true,
+    tabs: true,
+    tasklist: true,
+    vPre: true,
+    highlighter: {
+      collapsedLines: true,
+      type: "shiki", // or "prismjs"
+      notationDiff: true
+      // shiki 或 prismjs 选项
+      // ...
     },
 
-    // 加密配置
-    encrypt: {
-        config: {
-            '/demo/encrypt.html': {
-                hint: 'Password: 1234',
-                password: '1234',
-            },
-            '/learn/Project/5.html': {
-                hint: "机密",
-                password: "112700"
-            },
-            '/learn/Project/6.html': {
-                hint: "机密",
-                password: "112700"
-            },
-            '/learn/Project/7.html': {
-                hint: "机密",
-                password: "112700"
-            }
-        },
+    // 取消注释它们如果你需要 TeX 支持
+    // math: {
+    //   // 启用前安装 katex
+    //   type: "katex",
+    //   // 或者安装 mathjax-full
+    //   type: "mathjax",
+    // },
+
+    // 如果你需要幻灯片，安装 @vuepress/plugin-revealjs 并取消下方注释
+    // revealjs: {
+    //   plugins: ["highlight", "math", "search", "notes", "zoom"],
+    // },
+
+    // 在启用之前安装 chart.js
+    // chartjs: true,
+
+    // insert component easily
+
+    // 在启用之前安装 echarts
+    // echarts: true,
+
+    // 在启用之前安装 flowchart.ts
+    // flowchart: true,
+
+    // 在启用之前安装 mermaid
+    // mermaid: true,
+
+    // playground: {
+    //     presets: ["ts", "vue"],
+    // },
+
+    // 在启用之前安装 @vue/repl
+    vuePlayground: true,
+
+    // 在启用之前安装 sandpack-vue3
+    sandpack: true,
+  },
+
+  // 在这里配置主题提供的插件
+  plugins: {
+    blog: true,
+
+    // 启用之前需安装 @waline/client
+    // 警告: 这是一个仅供演示的测试服务，在生产环境中请自行部署并使用自己的服务！
+    // comment: {
+    //   provider: "Waline",
+    //   serverURL: "https://waline-comment.vuejs.press",
+    // },
+    slimsearch: {},
+
+    components: {
+      components: ['Badge', 'VPCard'],
     },
 
-    // 多语言配置
-    metaLocales: {
-        editLink: '在 GitHub 上编辑此页',
+    icon: {
+      prefix: 'fa6-solid:',
     },
 
-    // 如果想要实时查看任何改变，启用它。注: 这对更新性能有很大负面影响
-    // hotReload: true,
-
-    // 此处开启了很多功能用于演示，你应仅保留用到的功能。
-    markdown: {
-        align: true,
-        attrs: true,
-        codeTabs: true,
-        component: true,
-        demo: true,
-        figure: true,
-        gfm: true,
-        imgLazyload: true,
-        imgSize: true,
-        include: true,
-        mark: true,
-        plantuml: true,
-        spoiler: true,
-        stylize: [
-            {
-                matcher: 'Recommended',
-                replacer: ({tag}) => {
-                    if (tag === 'em')
-                        return {
-                            tag: 'Badge',
-                            attrs: {type: 'tip'},
-                            content: 'Recommended',
-                        }
-                },
-            },
-        ],
-        sub: true,
-        sup: true,
-        tabs: true,
-        tasklist: true,
-        vPre: true,
-        highlighter: {
-            collapsedLines: true,
-            type: "shiki", // or "prismjs"
-            notationDiff: true
-            // shiki 或 prismjs 选项
-            // ...
-        },
-
-        // 取消注释它们如果你需要 TeX 支持
-        // math: {
-        //   // 启用前安装 katex
-        //   type: "katex",
-        //   // 或者安装 mathjax-full
-        //   type: "mathjax",
-        // },
-
-        // 如果你需要幻灯片，安装 @vuepress/plugin-revealjs 并取消下方注释
-        // revealjs: {
-        //   plugins: ["highlight", "math", "search", "notes", "zoom"],
-        // },
-
-        // 在启用之前安装 chart.js
-        // chartjs: true,
-
-        // insert component easily
-
-        // 在启用之前安装 echarts
-        // echarts: true,
-
-        // 在启用之前安装 flowchart.ts
-        // flowchart: true,
-
-        // 在启用之前安装 mermaid
-        // mermaid: true,
-
-        // playground: {
-        //     presets: ["ts", "vue"],
-        // },
-
-        // 在启用之前安装 @vue/repl
-        vuePlayground: true,
-
-        // 在启用之前安装 sandpack-vue3
-        sandpack: true,
-    },
-
-    // 在这里配置主题提供的插件
-    plugins: {
-        blog: true,
-
-        // 启用之前需安装 @waline/client
-        // 警告: 这是一个仅供演示的测试服务，在生产环境中请自行部署并使用自己的服务！
-        // comment: {
-        //   provider: "Waline",
-        //   serverURL: "https://waline-comment.vuejs.press",
-        // },
-        slimsearch: {},
-
-        components: {
-            components: ['Badge', 'VPCard'],
-        },
-
-        icon: {
-            prefix: 'fa6-solid:',
-        },
-
-        // 如果你需要 PWA。安装 @vuepress/plugin-pwa 并取消下方注释
-        // pwa: {
-        //   favicon: "/favicon.ico",
-        //   cacheHTML: true,
-        //   cacheImage: true,
-        //   appendBase: true,
-        //   apple: {
-        //     icon: "/assets/icon/apple-icon-152.png",
-        //     statusBarColor: "black",
-        //   },
-        //   msTile: {
-        //     image: "/assets/icon/ms-icon-144.png",
-        //     color: "#ffffff",
-        //   },
-        //   manifest: {
-        //     icons: [
-        //       {
-        //         src: "/assets/icon/chrome-mask-512.png",
-        //         sizes: "512x512",
-        //         purpose: "maskable",
-        //         type: "image/png",
-        //       },
-        //       {
-        //         src: "/assets/icon/chrome-mask-192.png",
-        //         sizes: "192x192",
-        //         purpose: "maskable",
-        //         type: "image/png",
-        //       },
-        //       {
-        //         src: "/assets/icon/chrome-512.png",
-        //         sizes: "512x512",
-        //         type: "image/png",
-        //       },
-        //       {
-        //         src: "/assets/icon/chrome-192.png",
-        //         sizes: "192x192",
-        //         type: "image/png",
-        //       },
-        //     ],
-        //     shortcuts: [
-        //       {
-        //         name: "Demo",
-        //         short_name: "Demo",
-        //         url: "/demo/",
-        //         icons: [
-        //           {
-        //             src: "/assets/icon/guide-maskable.png",
-        //             sizes: "192x192",
-        //             purpose: "maskable",
-        //             type: "image/png",
-        //           },
-        //         ],
-        //       },
-        //     ],
-        //   },
-        // },
-    },
+    // 如果你需要 PWA。安装 @vuepress/plugin-pwa 并取消下方注释
+    // pwa: {
+    //   favicon: "/favicon.ico",
+    //   cacheHTML: true,
+    //   cacheImage: true,
+    //   appendBase: true,
+    //   apple: {
+    //     icon: "/assets/icon/apple-icon-152.png",
+    //     statusBarColor: "black",
+    //   },
+    //   msTile: {
+    //     image: "/assets/icon/ms-icon-144.png",
+    //     color: "#ffffff",
+    //   },
+    //   manifest: {
+    //     icons: [
+    //       {
+    //         src: "/assets/icon/chrome-mask-512.png",
+    //         sizes: "512x512",
+    //         purpose: "maskable",
+    //         type: "image/png",
+    //       },
+    //       {
+    //         src: "/assets/icon/chrome-mask-192.png",
+    //         sizes: "192x192",
+    //         purpose: "maskable",
+    //         type: "image/png",
+    //       },
+    //       {
+    //         src: "/assets/icon/chrome-512.png",
+    //         sizes: "512x512",
+    //         type: "image/png",
+    //       },
+    //       {
+    //         src: "/assets/icon/chrome-192.png",
+    //         sizes: "192x192",
+    //         type: "image/png",
+    //       },
+    //     ],
+    //     shortcuts: [
+    //       {
+    //         name: "Demo",
+    //         short_name: "Demo",
+    //         url: "/demo/",
+    //         icons: [
+    //           {
+    //             src: "/assets/icon/guide-maskable.png",
+    //             sizes: "192x192",
+    //             purpose: "maskable",
+    //             type: "image/png",
+    //           },
+    //         ],
+    //       },
+    //     ],
+    //   },
+    // },
+  },
 })
